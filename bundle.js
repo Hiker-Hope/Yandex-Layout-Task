9f1eb5ba19fc8e40e6eed11f3d6729dde9af9704
Hope R <hope_r@Hopes-MacBook-Pro.local>
2018-08-07 03:10:46 +0300

    Add devices tiles via js

--- bundle.js
+++ bundle.js
@@ -1,138 +1,3 @@
-// Devices menu - mobile
-
-const toggleDevicesMenuButtons = document.querySelectorAll('.devices-menu-item__label')
-for(i=0; i<toggleDevicesMenuButtons.length; i++) {
-    const toggleDevicesMenuButton = toggleDevicesMenuButtons[i]
-    toggleDevicesMenuButton.addEventListener('click', function() {
-        toggleDevicesMenuButton.checked = true;
-        document.querySelector('.devices-section__menu').classList.toggle('devices-section__menu--hidden')
-    })
-}
-
-// Modals
-
-function popUpShow(itemId) {
-    document.querySelector('.overlay').classList.remove('overlay--hidden');
-    document.querySelector('.modal-container').classList.remove('modal-container--hidden');
-    document.getElementById(itemId).classList.remove('hidden')
-}
-
-function popUpHide() {
-    document.querySelector('.overlay').classList.add('overlay--hidden');
-    document.querySelector('.modal-container').classList.add('modal-container--hidden');
-    let tileClose = document.querySelectorAll('.modal__main')
-        for (i=0; i<tileClose.length; i++) {
-            tileClose[i].classList.add('hidden')
-        } 
-}
-
-const mainContainer = document.querySelector('.main')
-mainContainer.addEventListener('click', function({ target }) {
-    if(!target) return
-
-    const item = target.closest('.item')
-    if (item.classList.contains('lightBulb')) popUpShow('home-lights')
-    if (item.classList.contains('thermometer')) popUpShow('home-temperature')
-    if (item.classList.contains('floors')) popUpShow('home-floor')
-})
-
-const modal = document.querySelector('.modal-container')
-modal.addEventListener('click', function({ target }) {
-        if(!target) return
-        if (target.closest('.modal-item__button')) popUpHide()
-})
-
-// // Hamburger menu
-
-// const toggleMainMenuBtn = document.querySelector('.header-menu__icon')
-// toggleMainMenuBtn.addEventListener('click', function() {
-//     document.querySelector('.header-menu').classList.toggle('header-menu--hidden')
-// })
-
-// // Forward button disable
-
-// function shouldEnableForward(parentSelector) {
-//     const forward = document.querySelector(`${parentSelector} .paginator--forward`)
-//     const container = document.querySelector(`${parentSelector} > div:nth-of-type(2)`)
-//     forward.disabled = container.offsetWidth >= container.scrollWidth
-// }
-
-// shouldEnableForward('.main__section--selected')
-// shouldEnableForward('.main__section--devices')
-
-// // Pagination
-
-// function turnPage(direction, container) {
-//     let { offsetWidth, style: { transform: transformStyle } } = container
-//     const translateValue = Number(transformStyle.replace(/[^\d.-]/g, ''));
-//     const nextTranslateValue = direction == 'forward' ?
-//                         translateValue - offsetWidth :
-//                         translateValue + offsetWidth
-    
-//     container.style.transform = `translateX(${nextTranslateValue}px)`
-// }
-
-// const paginatorContainerMain = document.querySelector('.main')
-// paginatorContainerMain.addEventListener('click', function({ target }) {
-//     if(!target) return;
-
-//     const paginatorSection = target.closest('.main__section')
-//     const itemToMove =
-//         paginatorSection.classList.contains('main__section--selected') ?
-//                             document.querySelector('.selected-list') :
-//                             document.querySelector('.devices-list')
-
-//     const paginatorButton = target.closest('.paginator-button')
-//         if (paginatorButton.classList.contains('paginator--forward')) {
-//             const backButton = paginatorButton.parentNode.querySelector('.paginator--back')
-//             backButton.disabled = false
-
-//             turnPage ('forward', itemToMove) 
-//             const { offsetWidth, scrollWidth, style: { transform: transformStyle } } = itemToMove
-//             const translateValue = Number(transformStyle.replace(/[^\d.]/g, ''));
-//             paginatorButton.disabled = translateValue + offsetWidth >= scrollWidth
-//         }
-
-//         if (paginatorButton.classList.contains('paginator--back')) {
-//             const forwardButton = paginatorButton.parentNode.querySelector('.paginator--forward')
-//             forwardButton.disabled = false
-
-//             turnPage ('back', itemToMove)  
-//             const { style: { transform: transformStyle } } = itemToMove
-//             const translateValue = Number(transformStyle.replace(/[^\d.-]/g, ''));
-//             paginatorButton.disabled = translateValue >= 0
-//         }
-// })
-
-// // Temperature range slider
-
-// function getOutputValue(temperatureValue) {   
-//     const outputValue = 
-//             (temperatureValue > 0) ? 
-//             `+${temperatureValue}` :
-//             temperatureValue 
-//         document.getElementById('temperature--output').textContent = outputValue;
-// }
-
-// // Modal settings menu
-
-// const modalContainer = document.querySelector('.modal-container')
-// modalContainer.addEventListener('click', function({ target }) {
-//     if(!target) return;
-
-//     const modalMain = target.closest('.modal__main')
-//     const menuButton = target.closest('.menu-section__item')
-//     const lightsSlider = document.getElementById('range-lights')
-//     const temperatureSlider = document.getElementById('range-temperature')    
-    
-//     if (modalMain.classList.contains('modal--lights')) {
-//         lightsSlider.value = menuButton.value
-//     } else { 
-//         temperatureSlider.value = menuButton.value
-//         getOutputValue(temperatureSlider.value)
-//     } 
-// })
-
 // Devices list
 
 const devices = [
@@ -193,26 +58,195 @@ const devices = [
     }
 ]
 
-const devicesHTML = devices.map((device) => 
-`<div class="item ${device.type} ${device.room}">
-    <img class="item__icon" src="${device.icon}">
-    <div class="item__content">
-        <h4 class="emphasized-text text--element-title">${device.name}</h4>
-            {% if (${device.isOn}) %}
-                <p class="item__text text--content">Включено</p>
-            {% else %}
-                <p class="item__text text--content">Выключено</p>
-            {% endif %}
-    </div>
-</div>` )
+const devicesHTML = devices.map(device => document.createElement('div'))
+devicesHTML.forEach(deviceHTML => deviceHTML.className = 'item')
+let device = {};
+let deviceHTML;
+for (i=0; i<devices.length; i++) {
+
+    device = devices[i]
+    deviceHTML = devicesHTML[i] 
+    deviceHTML.classList.add(device.type, device.room)
+
+    const icon = document.createElement('img')
+        icon.classList.add('item__icon')
+        icon.setAttribute('src', device.icon)
+    deviceHTML.appendChild(icon)    
+
+    const content = document.createElement('div')
+        content.classList.add('item__content')
+    deviceHTML.appendChild(content)    
+
+    const title = document.createElement('h4')
+        title.classList.add('emphasized-text', 'text--element-title')
+        title.textContent = device.name
+    content.appendChild(title)
+
+    const text = document.createElement('p')
+        text.classList.add('item__text', 'text--content')
+        text.textContent
+                = device.isOn ?
+                'Включено' : 
+                'Выключено'
+    content.appendChild(text)
+
+}
+// `<img class="item__icon" src="${device.icon}">
+//     <div class="item__content">
+//         <h4 class="emphasized-text text--element-title">${device.name}</h4>
+//             {% if (${device.isOn}) %}
+//                 <p class="item__text text--content">Включено</p>
+//             {% else %}
+//                 <p class="item__text text--content">Выключено</p>
+//             {% endif %}
+//      </div>` 
+
+console.log(devicesHTML)
 
 const devicesSection = document.querySelector('.main__section--devices')
 const devicesList = document.createElement('div')
 devicesList.className = 'devices-list'
 devicesSection.appendChild(devicesList)
-
-/// FIX HERE vvvv 
-
 devicesHTML.forEach((deviceHTML) => devicesList.appendChild(deviceHTML))
 
 // Devices filters
+
+
+
+
+
+
+// Devices menu - mobile
+
+const toggleDevicesMenuButtons = document.querySelectorAll('.devices-menu-item__label')
+for(i=0; i<toggleDevicesMenuButtons.length; i++) {
+    const toggleDevicesMenuButton = toggleDevicesMenuButtons[i]
+    toggleDevicesMenuButton.addEventListener('click', function() {
+        toggleDevicesMenuButton.checked = true;
+        document.querySelector('.devices-section__menu').classList.toggle('devices-section__menu--hidden')
+    })
+}
+
+// Modals
+
+function popUpShow(itemId) {
+    document.querySelector('.overlay').classList.remove('overlay--hidden');
+    document.querySelector('.modal-container').classList.remove('modal-container--hidden');
+    document.getElementById(itemId).classList.remove('hidden')
+}
+
+function popUpHide() {
+    document.querySelector('.overlay').classList.add('overlay--hidden');
+    document.querySelector('.modal-container').classList.add('modal-container--hidden');
+    let tileClose = document.querySelectorAll('.modal__main')
+        for (i=0; i<tileClose.length; i++) {
+            tileClose[i].classList.add('hidden')
+        } 
+}
+
+const mainContainer = document.querySelector('.main')
+mainContainer.addEventListener('click', function({ target }) {
+    if(!target) return
+
+    const item = target.closest('.item')
+    if (item.classList.contains('lightBulb')) popUpShow('home-lights')
+    if (item.classList.contains('thermometer')) popUpShow('home-temperature')
+    if (item.classList.contains('floors')) popUpShow('home-floor')
+})
+
+const modal = document.querySelector('.modal-container')
+modal.addEventListener('click', function({ target }) {
+        if(!target) return
+        if (target.closest('.modal-item__button')) popUpHide()
+})
+
+// Hamburger menu
+
+const toggleMainMenuBtn = document.querySelector('.header-menu__icon')
+toggleMainMenuBtn.addEventListener('click', function() {
+    document.querySelector('.header-menu').classList.toggle('header-menu--hidden')
+})
+
+// Forward button disable
+
+function shouldEnableForward(parentSelector) {
+    const forward = document.querySelector(`${parentSelector} .paginator--forward`)
+    const container = document.querySelector(`${parentSelector} > div:nth-of-type(2)`)
+    forward.disabled = container.offsetWidth >= container.scrollWidth
+}
+
+shouldEnableForward('.main__section--selected')
+shouldEnableForward('.main__section--devices')
+
+// Pagination
+
+function turnPage(direction, container) {
+    let { offsetWidth, style: { transform: transformStyle } } = container
+    const translateValue = Number(transformStyle.replace(/[^\d.-]/g, ''));
+    const nextTranslateValue = direction == 'forward' ?
+                        translateValue - offsetWidth :
+                        translateValue + offsetWidth
+    
+    container.style.transform = `translateX(${nextTranslateValue}px)`
+}
+
+const paginatorContainerMain = document.querySelector('.main')
+paginatorContainerMain.addEventListener('click', function({ target }) {
+    if(!target) return;
+
+    const paginatorSection = target.closest('.main__section')
+    const itemToMove =
+        paginatorSection.classList.contains('main__section--selected') ?
+                            document.querySelector('.selected-list') :
+                            document.querySelector('.devices-list')
+
+    const paginatorButton = target.closest('.paginator-button')
+        if (paginatorButton.classList.contains('paginator--forward')) {
+            const backButton = paginatorButton.parentNode.querySelector('.paginator--back')
+            backButton.disabled = false
+
+            turnPage ('forward', itemToMove) 
+            const { offsetWidth, scrollWidth, style: { transform: transformStyle } } = itemToMove
+            const translateValue = Number(transformStyle.replace(/[^\d.]/g, ''));
+            paginatorButton.disabled = translateValue + offsetWidth >= scrollWidth
+        }
+
+        if (paginatorButton.classList.contains('paginator--back')) {
+            const forwardButton = paginatorButton.parentNode.querySelector('.paginator--forward')
+            forwardButton.disabled = false
+
+            turnPage ('back', itemToMove)  
+            const { style: { transform: transformStyle } } = itemToMove
+            const translateValue = Number(transformStyle.replace(/[^\d.-]/g, ''));
+            paginatorButton.disabled = translateValue >= 0
+        }
+})
+
+// Temperature range slider
+
+function getOutputValue(temperatureValue) {   
+    const outputValue = 
+            (temperatureValue > 0) ? 
+            `+${temperatureValue}` :
+            temperatureValue 
+        document.getElementById('temperature--output').textContent = outputValue;
+}
+
+// Modal settings menu
+
+const modalContainer = document.querySelector('.modal-container')
+modalContainer.addEventListener('click', function({ target }) {
+    if(!target) return;
+
+    const modalMain = target.closest('.modal__main')
+    const menuButton = target.closest('.menu-section__item')
+    const lightsSlider = document.getElementById('range-lights')
+    const temperatureSlider = document.getElementById('range-temperature')    
+    
+    if (modalMain.classList.contains('modal--lights')) {
+        lightsSlider.value = menuButton.value
+    } else { 
+        temperatureSlider.value = menuButton.value
+        getOutputValue(temperatureSlider.value)
+    } 
+})
